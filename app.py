d10ea040f09319d793408f72228c6d08f1afec3e
changjia <changjiazhu@gmail.com>
2024-01-17 19:04:36 -0500

    init

--- app.py
+++ app.py
@@ -1,43 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import subprocess
-import netifaces as ni
 
 app = Flask(__name__)
-
-@app.route('/network_info', methods=['GET'])
-def get_network_info():
-    # Assuming 'eth0' as the primary interface
-    interface = 'eth0'
-    print(f"Fetching network info for interface: {interface}")
-    try:
-        mac_address = ni.ifaddresses(interface)[ni.AF_LINK][0]['addr']
-        ip_address = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
-        print(f"MAC: {mac_address}, IP: {ip_address}")
-        return jsonify({'mac_address': mac_address, 'ip_address': ip_address})
-    except KeyError:
-        error_message = 'Unable to fetch network information'
-        print(error_message)
-        return jsonify({'error': error_message}), 500
-
-def execute_ping(host):
-    print(f"Pinging {host}...")
-    try:
-        output = subprocess.check_output(["ping", "-c", "1", host], stderr=subprocess.STDOUT, universal_newlines=True)
-        print(f"Ping output: {output}")
-        return output
-    except subprocess.CalledProcessError as e:
-        error_message = f"Ping failed: {e.output}"
-        print(error_message)
-        return error_message
-
-@app.route('/ping', methods=['GET'])
-def ping():
-    hosts = request.args.get('hosts', '169.254.130.1')  # Default hosts to ping
-    results = {}
-    for host in hosts.split(','):
-        print(f"Received ping request for host: {host}")
-        results[host] = execute_ping(host)
-    return jsonify(results)
 
 def execute_command(command):
     try:
@@ -46,14 +10,15 @@ def execute_command(command):
     except subprocess.CalledProcessError as e:
         return f"An error occurred: {e.stderr.decode('utf-8').strip()}"
 
-@app.route('/neigh', methods=['GET'])
+@app.route('/network_info', methods=['GET'])
 def get_network_info():
+    ip_addr_output = execute_command("ip addr")
     ip_neigh_output = execute_command("ip neigh")
     return jsonify({
+        'ip_addr': ip_addr_output,
         'ip_neigh': ip_neigh_output
     })
 
-
 if __name__ == '__main__':
-    print("Starting Flask app...")
     app.run(host='0.0.0.0', port=80)
+
